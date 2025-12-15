4ad242208545037a8b53aa160c3556fb5f8d5eb3
revanthreddyb14 <revanthreddyb14@gmail.com>
2025-12-15 12:09:18 +0530

    first github-actions file

---
+++ github-actions-ci-cd/app.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.end("🚀 App deployed using GitHub Actions CI/CD!");
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
+
