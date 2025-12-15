fcaf5c1382fe7894be3fb0376ab46d3a219462a3
root <root@ip-172-31-2-91.ap-south-1.compute.internal>
2025-12-15 07:13:10 +0000

    app

--- github-actions-ci-cd/app.js
+++ github-actions-ci-cd/app.js
@@ -1,10 +1,11 @@
+
 const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.end("🚀 App deployed using GitHub Actions CI/CD!");
 });
 
-server.listen(3000, () => {
+server.listen(3000, '0.0.0.0', () => {
   console.log("Server running on port 3000");
 });
 
